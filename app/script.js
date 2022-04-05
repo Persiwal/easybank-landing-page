ac2ea06ba135509cb87a0e17332a89d824dbff3d
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-04-05 21:42:57 +0200

    Create animated mobile menu

--- app/script.js
+++ app/script.js
@@ -1,5 +1,19 @@
 const hamburger = document.querySelector('.header__hamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('open');
+    header.classList.toggle('open'); // toggle hamburger menu
+    overlay.classList.toggle('fade-out');
+    body.classList.toggle('no-scroll');
+})
+
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth > 899) {
+        header.classList.remove('open'); // toggle hamburger menu
+        overlay.classList.add('fade-out');
+        body.classList.remove('no-scroll');
+    } 
 })
